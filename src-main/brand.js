bbdfbbdbcda4e7ff29c4f432d13d96aeb594a188
katboizz0-arch <katboizz0-arch@gmail.com>
2026-09-18 20:35:05 +0700

    smth

--- src-main/brand.js
+++ src-main/brand.js
@@ -1,6 +1,6 @@
-const PLATFORM_NAME = 'TurboWarp';
-const APP_NAME = 'TurboWarp Desktop';
-const PACKAGER_NAME = 'TurboWarp Packager';
+const PLATFORM_NAME = 'Aftercode';
+const APP_NAME = 'Aftercode Desktop';
+const PACKAGER_NAME = 'Aftercode Packager';
 
 module.exports = {
   PLATFORM_NAME,
